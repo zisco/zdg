b8b5282ae43d1c3320f783ba44fa9fbade0a8fbf
zisco <ziscos@gmail.com>
2012-10-07 18:00:04 +0200

    source tree created

---
+++ zdg/src/main/java/org/zisco/zdg/App.java
@@ -0,0 +1,13 @@
+package org.zisco.zdg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
